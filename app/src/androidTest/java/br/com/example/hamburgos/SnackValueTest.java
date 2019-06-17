a0718538c4381b0d034cdff985faf8ae8d9b02f3
Daniela Schwab <danyschwab@gmail.com>
2019-06-16 23:21:19 -0300

    Implementing tests

---
+++ app/src/androidTest/java/br/com/example/hamburgos/SnackValueTest.java
@@ -0,0 +1,67 @@
+package br.com.example.hamburgos;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.com.example.hamburgos.ui.MainActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class SnackValueTest {
+
+    /**
+     * Use {@link ActivityScenarioRule} to create and launch the activity under test, and close it
+     * after test completes. This is a replacement for {@link androidx.test.rule.ActivityTestRule}.
+     */
+    @Rule public ActivityScenarioRule<MainActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+
+
+    public static Matcher<View> nthChildOf(final Matcher<View> parentMatcher, final int childPosition) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with " + childPosition + " child view of type parentMatcher");
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view.getParent() instanceof ViewGroup)) {
+                    return parentMatcher.matches(view.getParent());
+                }
+
+                ViewGroup group = (ViewGroup) view.getParent();
+                return parentMatcher.matches(view.getParent()) && group.getChildAt(childPosition).equals(view);
+            }
+        };
+    }
+
+
+    @Test
+    public void checkValueForXBacon() {
+
+        onView(nthChildOf(withId(R.id.list_snacks), 0))
+                .check(matches(withText("X-Bacon")));
+        onView(nthChildOf(withId(R.id.list_snacks), 0))
+                .check(matches(withText("X-Bacon")));
+
+
+    }
+
+}
