08c01e4bc95c5e5a74e2402eff3953938830e64a
Daniela Schwab <danyschwab@gmail.com>
2019-06-24 15:53:47 -0300

    Defining new architectures

---
+++ mvvm/app/src/main/java/br/com/example/hamburgos/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.example.hamburgos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
